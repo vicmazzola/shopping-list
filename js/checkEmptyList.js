432c2adeceb604e4bd4b332666c6abcc86ae730e
Victor Mazzola <victormazzola2019@gmail.com>
2024-11-22 17:19:29 -0300

    need fix container main

---
+++ js/checkEmptyList.js
@@ -0,0 +1,10 @@
+const emptyListMessage = document.getElementById("empty-list-message");
+
+export function checkEmptyList(list) {
+    console.log(list);
+    if (list.querySelectorAll("li").length === 0) {
+        emptyListMessage.style.display = "block";
+    } else {
+        emptyListMessage.style.display = "none";
+    }
+}
